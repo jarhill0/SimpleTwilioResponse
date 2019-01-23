b142720114c5d8c01ea5a3af26f5d231cfd316be
jarhill0 <jarhill0@users.noreply.github.com>
2019-01-22 21:48:30 -0800

    Support audio files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from os.path import dirname, join
+from os.path import dirname, join, splitext
 
 from flask import Flask, render_template, request
 from twilio.twiml.voice_response import VoiceResponse
@@ -10,23 +10,30 @@ app = Flask(__name__)
 
 class SavedResponse:
     """Keeps a response in sync with disk, lazily loaded."""
-    FILE_PATH = join(dirname(__file__), 'static', 'response.txt')
+    FOLDER = join(dirname(__file__), 'static')
+    TEXT_PATH = join(FOLDER, 'response.txt')
+    AUDIO_PATH = join(FOLDER, 'audio.mp3')
+
+    @staticmethod
+    def audio_url():
+        return '/static/audio.mp3'
 
     def __init__(self):
-        self._resp = None
+        self._text = self._audio = None
+        self.use_text = True
 
     @property
     def text(self):
-        if self._resp is None:
-            with open(self.FILE_PATH) as f:
-                self._resp = f.read()
-        return self._resp
+        if self._text is None:
+            with open(self.TEXT_PATH) as f:
+                self._text = f.read()
+        return self._text
 
     @text.setter
     def text(self, value):
-        with open(self.FILE_PATH, 'w') as f:
+        with open(self.TEXT_PATH, 'w') as f:
             f.write(value)
-        self._resp = value
+        self._text = value
 
 
 RESPONSE = SavedResponse()
@@ -36,21 +43,40 @@ RESPONSE = SavedResponse()
 def voice():
     """Respond to incoming phone calls."""
     resp = VoiceResponse()
-    resp.say(RESPONSE.text)
+    if RESPONSE.use_text:
+        resp.say(RESPONSE.text)
+    else:
+        resp.play(url=RESPONSE.audio_url())
     return str(resp)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def edit_message():
-    saved = False
-    valid_pass = True
+    error = ''
+    success = ''
     if request.method == 'POST':
-        valid_pass = request.values.get('pw', '') == config_secrets.password
-        if valid_pass and 'mess' in request.values:
-            new_response = request.values['mess']
-            RESPONSE.text = new_response
-            saved = True
-    return render_template('editor.html', message=RESPONSE.text, saved=saved, valid_pass=valid_pass)
+        if request.values.get('pw', '') == config_secrets.password and 'mess' in request.values:
+            if request.values.get('use-audio'):
+                if 'audio-file' not in request.files:
+                    error = 'No file provided.'
+                else:
+                    file = request.files['audio-file']
+                    if not file.filename:
+                        error = 'Empty file.'
+                    elif not splitext(file.filename)[1].lower() == '.mp3':
+                        error = 'Invalid file type. Only MP3 is supported.'
+                    else:
+                        file.save(RESPONSE.AUDIO_PATH)
+                        success = 'The new audio message has been set.'
+                        RESPONSE.use_text = False
+            else:
+                new_response = request.values['mess']
+                RESPONSE.text = new_response
+                RESPONSE.use_text = True
+                success = 'The new text message has been set.'
+        else:
+            error = 'Invalid password.'
+    return render_template('editor.html', message=RESPONSE.text, success=success, error=error)
 
 
 if __name__ == "__main__":
